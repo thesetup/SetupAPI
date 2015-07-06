a0d9d5668dc90431a89b2089dfdf8c901509001c
TuckerWatts <tuckerrwatts@gmail.com>
2015-07-06 16:00:02 -0400

    added users table/controller/routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,12 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  post 'users/register', to: 'users#register'
+  get 'users/:user_id', to: 'users#show'
+  get 'users', to: 'users#index'
+
+  post 'users/login', to: 'users#login'
+
   # Example resource route with options:
   #   resources :products do
   #     member do
