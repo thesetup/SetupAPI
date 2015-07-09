0a9297e91f84c2d1adeb604bdfaac4da933209b5
TuckerWatts <tuckerrwatts@gmail.com>
2015-07-09 11:40:11 -0400

    quick fix - made email column in user table unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709140620) do
+ActiveRecord::Schema.define(version: 20150709153805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20150709140620) do
     t.datetime "updated_at",   null: false
     t.string   "username"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "videos", force: :cascade do |t|
     t.integer  "videoable_id"
