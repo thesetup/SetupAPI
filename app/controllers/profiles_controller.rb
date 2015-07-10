0f44fbb894efe0768e094fd39b60c30666b59784
Trevor Terris <tjterris@gmail.com>
2015-07-10 14:32:44 -0400

    fixed bug that was not allowing users to create profiles for people with emails that were not theirs

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,10 @@ class ProfilesController < ApplicationController
   before_action :authenticate_with_token!
 
     def create
-      @user = User.find_by!(email: params[:email])
+      @user = User.create(email: params[:email],
+                          username: params[:username],
+                          password: params[:password])
+                          
       @profile = Profile.new(profilee_id: @user.id,
                              profiler_id: current_user.id)
         #### something isn't working with the validation to prevent a
@@ -15,6 +18,28 @@ class ProfilesController < ApplicationController
         render json: {errors: @profile.errors.full_messages},
                status: :unprocessable_entity
       end
+    end
+
+    def create_question
+
+      @profile = Profile.find_by(current_user.profile.id)
+      binding.pry
+      @question = @profile.questions.new(name: params[:name],
+                                        email: params[:email],
+                                        birthyear: params[:birthyear],
+                                        gender: params[:gender],
+                                        orientation: params[:orientation],
+                                        occupation: params[:occupation],
+                                        location: params[:location])
+
+        if @question.save
+          render json: @question.as_json,
+               status: :created
+        else
+          render json: {errors: @question.errors.full_messages},
+               status: :unprocessable_entity
+        end
+
     end
 
   def index
