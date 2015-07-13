8ff436cf1a2ca4dda74541442bf9856a4ca86cfe
Trevor Terris <tjterris@gmail.com>
2015-07-12 21:58:09 -0400

    I think I have the user / profile creation debugged, but need to return json for questions/videos/images data still

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,11 +2,10 @@ class Profile < ActiveRecord::Base
   belongs_to :user, foreign_key: 'profilee_id', class_name: User
   belongs_to :author, foreign_key: 'profiler_id', class_name: User
 
-  validates_uniqueness_of :profilee_id, scope: :profiler_id,
-                           message: 'cannot be same as Profiler.'
+  validates :profilee_id, uniqueness: { scope: :profiler_id}
 
 
-  validates_uniqueness_of :profiler_id, scope: :profilee_id
+  #validates_uniqueness_of :profiler_id, scope: :profilee_id
   ###currently a user is able to create a profile for himself
   has_many :questions
   has_many :videos, as: :videoable
