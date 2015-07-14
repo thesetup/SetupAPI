bb231c9ee56a10258c476d08c3ed1d4f8ffff761
Trevor Terris <tjterris@gmail.com>
2015-07-14 16:59:31 -0400

    all videos, images and questions should now be deleted when a profile is destroyed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,13 +2,14 @@ class Profile < ActiveRecord::Base
   belongs_to :user, foreign_key: 'profilee_id', class_name: User
   belongs_to :author, foreign_key: 'profiler_id', class_name: User
 
+
   validates :profilee_id, presence: {message: 'cannot be the same as Profiler'}, uniqueness: { scope: :profiler_id}
 
 
   #validates_uniqueness_of :profiler_id, scope: :profilee_id
   ###currently a user is able to create a profile for himself
-  has_many :questions
-  has_many :videos, as: :videoable
-  has_many :images, as: :imageable
+  has_many :questions, dependent: :destroy
+  has_many :videos, as: :videoable, dependent: :destroy
+  has_many :images, as: :imageable, dependent: :destroy
 
 end
