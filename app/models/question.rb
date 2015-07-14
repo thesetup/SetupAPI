cf5c98c73b2206dfa63513413b63b39fb9c5a8c0
Trevor Terris <tjterris@gmail.com>
2015-07-14 17:03:13 -0400

    users are now required to answer all questions before creating a profile.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :profile
-  # validates :name, :email, :birthyear, :gender, :orientation,
-  #           :occupation, :location, :profile_id, presence: true
+  validates :birthyear, :gender, :orientation,
+            :occupation, :location,  presence: true
 end
