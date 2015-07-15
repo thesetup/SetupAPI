05bfebf4dbf35bc8394291d5daceabe2bb46d975
Trevor Terris <tjterris@gmail.com>
2015-07-15 14:57:27 -0400

    added method to only allow 4 video uploads per profile.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,11 @@
 class Video < ActiveRecord::Base
   belongs_to :videoable, polymorphic: true
-  belongs_to :profile
 
-  validates_associated :profile
+  validate :max_video_count
+
+  def max_video_count
+    unless videoable.videos.count < 4
+      errors.add(:base, "Already have maximum number of videos")
+    end
+  end
 end
