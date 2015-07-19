0bf9768867e12278bbdf6f729b9b70aea6a2301b
Trevor Terris <tjterris@gmail.com>
2015-07-19 00:14:47 -0400

    Cleaning up Video model.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,3 @@
 class Video < ActiveRecord::Base
   belongs_to :videoable, polymorphic: true
-
-  # validate :max_video_count, :on => :create_video
-
-  # def max_video_count
-  #   if videoable.videos.count == 4
-  #     errors.add(:base, "Already have maximum number of videos")
-  #   end
-  # end
 end
