c012dda85b6554dd73c22bc4b5dca5e831c951aa
TuckerWatts <tuckerrwatts@gmail.com>
2015-07-20 13:40:17 -0400

    adding search functionality

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,12 @@
 class Question < ActiveRecord::Base
+  # include PgSearch
   belongs_to :profile
   validates :birthyear, :gender, :orientation, :occupation, :location,
              presence: true
+
+  pg_search_quote :search, :against => [:birthyear,
+                                        :gender,
+                                        :orientation,
+                                        :occupation,
+                                        :location]
 end
