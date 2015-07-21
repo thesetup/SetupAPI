b372e7b7f2a468d56c05bede096f478e36de299f
Trevor Terris <tjterris@gmail.com>
2015-07-21 17:44:51 -0400

    Tweaked some of jbuilder files.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,12 +2,20 @@ class QuestionsController < ApplicationController
   def search
     search_text = params[:keywords]
     @questions = Question.search(search_text)
+
+    @profiles = []
+    @questions.map do |q|
+      @profiles << q.profile
+    end
+
+    #@profiles = Profile.search(search_text)
     if @questions.present?
       render 'search.json.jbuilder', status: :ok
+      # render json: @questions.to_json(:include => :profile)
+
     else
-      render json: { message: "Oops! There might be a typo! Please search again" },
+      render json: { message: "Oops! There might be a typo! Please search again." },
              render: :not_found
     end
   end
 end
-
--- app/views/profiles/index.json.jbuilder
+++ app/views/profiles/index.json.jbuilder
@@ -2,7 +2,7 @@ json.profiles @profiles do |p|
   json.id p.id
   json.profiler_id p.profiler_id
   json.profilee_id p.profilee_id
-    json.avatar_url p.user.avatar.url
+  json.avatar_url p.user.avatar.url
 end
 
 json.questions @questions
