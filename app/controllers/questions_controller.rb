200b6f11e35b782e5c1feb430aef99879eca5800
Trevor Terris <tjterris@gmail.com>
2015-07-23 17:30:51 -0400

    Commented out search by age code.  We are not using that for the MVP demo.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,15 @@
 class QuestionsController < ApplicationController
   def search
     search_text = params[:keywords]
-    min_age = params[:min_age].to_i
-    max_age = params[:max_age].to_i
+    # min_age = params[:min_age].to_i
+    # max_age = params[:max_age].to_i
 
     # question_by_search = Question.search(search_text)
     # question_by_age = Question.where(birthyear: (DateTime.now.utc.year-max_age)..(DateTime.now.utc.year - min_age))
     # @questions = (question_by_search & question_by_age)
 
-    @questions = Question.search(search_text).where(birthyear: (DateTime.now.utc.year-max_age)..(DateTime.now.utc.year-min_age))
+    @questions = Question.search(search_text)
+    # .where(birthyear: (DateTime.now.utc.year-max_age)..(DateTime.now.utc.year-min_age))
     @profiles = []
     @questions.map do |q|
       @profiles << q.profile
